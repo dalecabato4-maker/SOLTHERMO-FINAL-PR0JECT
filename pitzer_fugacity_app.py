dcf9b03ba60ab55cd09f87a65e1ad4e2ec0648c6
dalecabato4-maker <dalecabato4@gmail.com>
2025-11-14 12:30:31 +0800

    Update pitzer_fugacity_app.py

--- pitzer_fugacity_app.py
+++ pitzer_fugacity_app.py
@@ -158,6 +158,7 @@ if calculate:
 st.markdown("""
 ---
 **References:**
-- Pitzer, K.S. & Curl, R.F. Jr. (1957). *J. Am. Chem. Soc.*, **79**, 2369.  
+- Pitzer, K.S. & Curl, R.F. Jr. (1957). *J. Am. Chem. Soc.*, 79, 2369.  
 - Smith, J.M., Van Ness, H.C., & Abbott, M.M. *Introduction to Chemical Engineering Thermodynamics* (8th Ed.).
-""")
+""", unsafe_allow_html=True)
+
