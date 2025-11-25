d82e5eba9c1ae1de6553e159e35e6086f2453fca
dalecabato4-maker <dalecabato4@gmail.com>
2025-11-25 13:19:22 +0800

    Update pitzer_fugacity_app.py

--- pitzer_fugacity_app.py
+++ pitzer_fugacity_app.py
@@ -362,7 +362,7 @@ if st.session_state.show_homepage:
     <div style="text-align:center; padding:40px;">
         <h1 style="font-size:52px;">⚗️ Fugacitor ⚗️</h1>
         <p style="font-size:18px; max-width:700px; margin:auto;">
-        <p class="montserrat">
+        <p class="playfair">
             Welcome to the Fugacity & Fugacity Coefficient Calculator using the <b>Pitzer correlation</b>.  
             Fugacity is a corrected pressure that accounts for non-ideal gas behavior — essential for accurate thermodynamic modeling.  
             This tool supports both pure gases and mixtures, and is based on the work of Pitzer & Curl.
