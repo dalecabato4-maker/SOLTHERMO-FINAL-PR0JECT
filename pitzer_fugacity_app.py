0082ded9c3d18d2a748391f97ca7cc09eff7d3dd
dalecabato4-maker <dalecabato4@gmail.com>
2025-12-05 22:54:02 +0800

    Update pitzer_fugacity_app.py

--- pitzer_fugacity_app.py
+++ pitzer_fugacity_app.py
@@ -369,7 +369,7 @@ if st.session_state.show_homepage:
     st.markdown("""
     <div style="text-align:center; padding:40px;">
         <img src="https://github.com/dalecabato4-maker/SOLTHERMO-FINAL-PR0JECT/blob/main/593456646_1014219404256349_7204573209995906307_n-removebg-preview.png?raw=true" width="600" style="margin-bottom:5px;" />
-        <h1 style="font-size:40px;">Fugacitor</h1>
+        <h1 style="font-size:40px; margin-top:0px;">Fugacitor</h1>
         <p style="font-size:18px; max-width:700px; margin:auto;">
         <p class="playfair">
             Welcome to the Fugacity & Fugacity Coefficient Calculator using the <b>Pitzer correlation</b>.  
