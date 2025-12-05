87d55544c4ce82ac67204fcb19857686f9a84d00
dalecabato4-maker <dalecabato4@gmail.com>
2025-12-05 23:04:47 +0800

    Update pitzer_fugacity_app.py

--- pitzer_fugacity_app.py
+++ pitzer_fugacity_app.py
@@ -350,7 +350,7 @@ if "loaded" not in st.session_state:
     with loading_placeholder:
         st.markdown("""
             <div style="text-align:center; padding:60px;">
-                <img src="https://github.com/dalecabato4-maker/SOLTHERMO-FINAL-PR0JECT/blob/main/Calculator%20(1).png?raw=true" width="400" style="margin-bottom:40px;" />
+                <img src="https://github.com/dalecabato4-maker/SOLTHERMO-FINAL-PR0JECT/blob/main/new%20logo.png?raw=true="600" style="margin-bottom:40px;" />
                 <h2 style="color:#1E88E5;">🔄 Loading Fugacity Calculator...</h2>
                 <p style="font-size:24px;">
                 <p class="playfair">
